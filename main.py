5a98410018d8509612508e28c3ea2f721d3795a9
Keonnartey <125210401+Keonnartey@users.noreply.github.com>
2023-09-12 15:41:52 -0700

    Updated main.py

--- main.py
+++ main.py
@@ -1,5 +1,28 @@
-def add(a, b):
-    return a + b
+import pandas as pd
+import matplotlib.pyplot as plt
 
-if __name__ == "__main__":
-    print(add(1, 3))
+
+def pandas_descriptive_stat_mean(df: pd.DataFrame, col: str) -> float:
+    return df[col].mean()
+
+def pandas_descriptive_stat_median(df: pd.DataFrame, col: str) -> float:
+    return df[col].median()
+
+def pandas_descriptive_stat_std(df: pd.DataFrame, col: str) -> float:
+    return df[col].std()
+
+def visualize_data(df):
+    plt.scatter(df["mpg"], df["hp"])
+    plt.xlabel("Miles Per Gallon")
+    plt.ylabel("Horse Power")
+    plt.title("Miles per gallon changes with automible weight")
+    plt.show()
+
+
+if __name__ == '__main__':
+    cars = pd.read_csv(r"https://gist.githubusercontent.com/seankross/a412dfbd88b3db70b74b/raw/5f23f993cd87c283ce766e7ac6b329ee7cc2e1d1/mtcars.csv")
+    print(cars.head())
+    print(pandas_descriptive_stat_mean(cars, 'mpg'))
+    print(pandas_descriptive_stat_median(cars, 'mpg'))
+    print(pandas_descriptive_stat_std(cars, 'mpg'))
+    visualize_data(cars)
